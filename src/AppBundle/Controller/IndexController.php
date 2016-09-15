5527eb43c564d652f57a6212234d8937233fae58
AntonMikheyev <banderaz90@gmail.com>
2016-09-15 10:35:32 -0700

    final commit

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Product;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,10 +11,35 @@ class IndexController extends Controller
 {
     public function indexAction(Request $request)
     {
-        echo 'TRETRETRE';
-        return '234';
-//        return $this->render('default/index.html.twig', [
-//            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-//        ]);
+        $products = $this->getDoctrine()->getRepository('AppBundle:Product')->findAll();
+        return $this->render('default/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+    public function categoriesPageAction($categoryName = null)
+    {
+        if (!is_null($categoryName)) {
+            $products = $this->getProductsByCategory($categoryName);
+        } else {
+            $products = $this->getDoctrine()->getRepository('AppBundle:Product')->findAll();
+        }
+        $categories = $this->getDoctrine()->getRepository('AppBundle:Category')->findAll();
+        return $this->render('default/categories.html.twig', [
+            'products' => $products,
+            'categories' => $categories,
+        ]);
+    }
+
+    private function getProductsByCategory($categoryName)
+    {
+        $category = $this->getDoctrine()->getRepository('AppBundle:Category')->findOneByName($categoryName);
+        $products = $category->getProducts();
+        $productsArray = [];
+        foreach ($products as $product) {
+            $productsArray[] = $product;
+        }
+
+        return $productsArray;
     }
 }
